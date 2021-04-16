ba53db3404646a8b9ab4c38cafa86b747706f7a3
Ray Njire <ray.njire23@gmail.com>
2021-04-16 21:42:15 +0300

    Updated the HTML copyright sign
    Signed-off-by: Ray Njire <ray.njire23@gmail.com>

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/Home.html');
+	exit;
+?>
+Something is wrong with the Website :-(
